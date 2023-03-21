94f9c768427f988a2839b7a83906e6cad572e6eb
RensOldenburger <115281629+RensOldenburger@users.noreply.github.com>
2023-03-21 12:57:14 +0100

    Add files via upload

---
+++ Assistive_Devices_Code_V1/h_bridge.h
@@ -0,0 +1,49 @@
+/*
+ * h_bridge.h - XvR 2020
+ */
+
+#ifndef _H_BRIDGE_H_
+#define _H_BRIDGE_H_
+
+// These pins are available on the shield via the header:
+//
+// digital 6	PH3
+// digital 7	PH4
+// digital 8	PH5
+// digital 9	PH6
+
+//
+
+// The settings below are for the Mega, modify
+// in case you want to use other pins
+//H_brug 1
+#define PORT_RPWM	PORTB
+#define PIN_RPWM	PB5     //( OC1A/PCINT5 )       digital pin 11
+#define DDR_RPWM	DDRB
+
+#define PORT_LPWM	PORTB
+#define PIN_LPWM	PB4     //( OC2A/PCINT4 )       digital pin 10
+#define DDR_LPWM	DDRB
+//H_brug 2
+#define PORT_RPWM1	PORTL
+#define PIN_RPWM1	PL3     //  digital pin 46
+#define DDR_RPWM1	DDRL
+
+#define PORT_LPWM1	PORTL
+#define PIN_LPWM1	PL4     //  digital pin 45
+#define DDR_LPWM1	DDRL
+//H_brug 3
+#define PORT_RPWM2	PORTH
+#define PIN_RPWM2   PH6     //( OC4A )              digital pin 9
+#define DDR_RPWM2	DDRH
+
+#define PORT_LPWM2	PORTF
+#define PIN_LPWM2	PF5     //( OC2B )               digital pin a5
+#define DDR_LPWM2	DDRF
+
+void init_h_bridge(void);
+void h_bridge_set_percentage(signed char percentage);
+void h_bridge_set_percentage1(signed char percentage);
+void h_bridge_set_percentage2(signed char percentage);
+
+#endif /* _H_BRIDGE_H_ */
---
+++ Assistive_Devices_Code_V1/main.c
@@ -0,0 +1,180 @@
+#ifndef F_CPU
+#define F_CPU 16000000ul
+#endif
+
+#include "h_bridge.h"
+#include "servo.h"
+
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/io.h>
+
+//schakelaars
+#define home_switch         PA0
+#define top_switch          PA1
+#define middel_switch       PA5
+
+//bediningpaneel
+#define home_knop           PA2
+#define opbouw_knop         PA3
+#define afbouw_knop         PA4
+#define volgende_steiger    PA6
+#define vorige_steiger      PA7
+
+
+void init(void)
+{
+	init_h_bridge();
+	init_servo();
+}
+
+int main(void)
+{
+	init();
+
+	// output
+	//bedingingpaneel
+	PORTA &= (1<<home_knop);
+	PORTA &= (1<<opbouw_knop);
+	PORTA &= (1<<afbouw_knop);
+	PORTA &= (1<<volgende_steiger);
+	PORTA &= (1<<vorige_steiger);
+	//schakelaars
+	PORTA &= (1<<home_switch);
+	PORTA &= (1<<top_switch);
+	PORTA &= (1<<middel_switch);
+	int status = 0;
+
+
+	while (1)
+	{
+		switch(status)
+		{
+        case 0:
+            if(!(PINA & (1<<home_switch)))
+            {
+                status = 2;
+                break;
+            }
+            if (PINA &(1<<home_switch))
+            {
+                if(!(PINA & (1<<home_knop)))
+                {
+                    status = 1;
+                    break;
+                }
+            }
+            break;
+        case 1:
+            if(!(PINA & (1<<home_switch)))
+            {
+                status = 0;
+                break;
+            }
+            h_bridge_set_percentage(-70);
+            break;
+        case 2:
+            if(!(PINA & (1<<opbouw_knop)))
+            {
+                status = 3;
+                break;
+            }
+            if(!(PINA & (1<<afbouw_knop)))
+            {
+                status = 4;
+                break;
+            }
+            break;
+
+            //Opbouwen
+        case 3:
+            servo1_set_percentage(50);
+            servo2_set_percentage(50);
+            status = 5;
+            break;
+        case 5:
+            if (!(PINA & (1<<top_switch)))
+            {
+                h_bridge_set_percentage(0);
+                status = 7;
+                break;
+            }
+            h_bridge_set_percentage(70);
+            break;
+        case 7:
+            if(!(PINA & (1<<volgende_steiger)))
+            {
+                status = 9;
+            }
+            break;
+        case 9:
+            if(!(PINA & (1<<middel_switch)))
+               {
+                   h_bridge_set_percentage(0);
+                   status = 11;
+                   break;
+               }
+               h_bridge_set_percentage(-70);
+               break;
+        case 11:
+            servo1_set_percentage(-50);
+            servo2_set_percentage(-50);
+            status = 13;
+            break;
+        case 13:
+            if (!(PINA & (1<<home_switch)))
+            {
+                h_bridge_set_percentage(0);
+                status = 2;
+                break;
+            }
+            h_bridge_set_percentage(-70);
+            break;
+        case 4:
+            if (!(PINA &(1<<middel_switch)))
+            {
+                h_bridge_set_percentage(0);
+                status = 6;
+                break;
+            }
+            h_bridge_set_percentage(70);
+            break;
+        case 6:
+            servo1_set_percentage(50);
+            servo2_set_percentage(50);
+            status = 8;
+            break;
+        case 8:
+            if(!(PINA & (1<<top_switch)))
+            {
+                h_bridge_set_percentage(0);
+                status = 10;
+                break;
+            }
+            h_bridge_set_percentage(70);
+            break;
+        case 10:
+            if(!(PINA &(1<<vorige_steiger)))
+            {
+                status = 12;
+            }
+            break;
+        case 12:
+            if(!(PINA & (1<<home_switch)))
+            {
+                h_bridge_set_percentage(0);
+                status = 14;
+                break;
+            }
+            h_bridge_set_percentage(-70);
+        case 14:
+            servo1_set_percentage(-50);
+            servo2_set_percentage(-50);
+            status = 2;
+            break;
+
+		}
+	}
+
+	return 0;
+}
