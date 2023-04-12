96db4b3c4dab555ef510b58f15d9e2242c151295
AldarvdWeck <128801480+AldarvdWeck@users.noreply.github.com>
2023-04-12 14:53:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-int buzz = 0;
+int State_verander = 1;
 
 void init(void)
 {
@@ -25,38 +25,27 @@ int main(void)
     PORTB |= (1 << PB7);
 	init();				                                // run init function
 
-	int status = 0;						                // status voor de switch case statement
+	int status = 1;						                // status voor de switch case statement
 
     knipperLichten(0);
     h_bridge_set_percentage(0);
     h_bridge_set_percentage2(0);
 
-    LCD_Scherm_Leeg();
-    LCD_Naar_Locatie(1, 1);
-    Stuur_LCD_String("Noodtoestand");
-
 	while (1)
 	{
-//        if (buzz >= 8)                              // buzzer gaat 8 keer aan en uit door timer 5
-//        {
-//            buzzer(0);                              // na 8 keer stopt de buzzer
-//            if (TIMSK5 == 0x00)                     // als timer 5 uit staat
-//            {
-//                buzz = 0;                           // wordt de buzzer gereset en kan opnieuw aangezet worden
-//            }
-//        }
-        if ((PINF & (1<<PF4)) == 0)//((PINF & (1<<PF4)) == 0)                // nood stop indrukken
+        if ((PINF & (1<<PF4)) == 0)//((PINF & (1<<PF4)) == 0)      // noodstop indrukken
         {
             DEBOUNCE;
             PORTF &= ~(1<<PF7);                     // als de noodstop is ingedrukt brandt het ledje
             status = 0;
         }
-        if (Bluetooth_Getal == 6)//((PINF & (1<<PF4)) == 0)                // nood stop indrukken
+        if (Bluetooth_Getal == 6)               // noodstop app indrukken
         {
             DEBOUNCE;
             PORTF &= ~(1<<PF7);                     // als de noodstop is ingedrukt brandt het ledje
             Bluetooth_Getal = 20;
             status = 99;
+            State_verander = status;
         }
 		switch(status)
 		{
@@ -65,17 +54,20 @@ int main(void)
             knipperLichten(1);
             h_bridge_set_percentage(0);
             h_bridge_set_percentage2(0);
-
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-            Stuur_LCD_String("Noodtoestand");
-
-            if ((PINF & (1<<PF4)) != 0)//((PINF & (1<<PF4)) != 0)     //Noodstop loslaten
+            if((State_verander != status) && (State_verander != 98))
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("!!Noodtoestand!!");
+                State_verander = 98;
+            }
+            if ((PINF & (1<<PF4)) != 0)     //Noodstop loslaten
             {
                 DEBOUNCE;
                 PORTF |= (1<<PF7);
                 Bluetooth_Getal = 20;                  // als de noodstop weer wordt losgelaten gaat het ledje uit
-                status = 1;
+                status = 666;
+                State_verander = status;
             }
             break;
         case 99: // nood_toestand
@@ -83,73 +75,153 @@ int main(void)
             knipperLichten(1);
             h_bridge_set_percentage(0);
             h_bridge_set_percentage2(0);
+            if(State_verander == status)
+            {
+                State_verander = status;
+            }
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("!!Noodtoestand!!");
+                State_verander = 100;
+            }
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-            Stuur_LCD_String("Noodtoestand");
-
-            if ((Bluetooth_Getal == 7) && ((PINF & (1<<PF4)) != 0))//((PINF & (1<<PF4)) != 0)     //Noodstop loslaten
+            if ((Bluetooth_Getal == 7) && ((PINF & (1<<PF4)) != 0))     //Noodstop loslaten
             {
                 DEBOUNCE;
                 PORTF |= (1<<PF7);
                 Bluetooth_Getal = 20;                  // als de noodstop weer wordt losgelaten gaat het ledje uit
-                status = 1;
+                status = 666;
+                State_verander = status;
             }
             break;
-        case 1: // init_state
+
+        case 666: //Keuze case na Noodtoestand
             buzzer(0);
             knipperLichten(0);
-            servo1_set_percentage(50);    // ingeklapt
-            servo2_set_percentage(0);    // ingeklapt
             h_bridge_set_percentage(0);
             h_bridge_set_percentage2(0);
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("Kies volgende");
+                LCD_Naar_Locatie(6, 2);
+                Stuur_LCD_String("actie");
+                State_verander = 100;
+            }
 
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Opstarten");
-
-            if (((PINF & (1<<in_uit_klappen)) == 0) || (Bluetooth_Getal == 8))//((PINF & (1<<in_uit_klappen)) == 0)
+            if (((PINF & (1<<opbouw_knop)) == 0) || (Bluetooth_Getal == 3))//Opbouwen
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 4;
+                State_verander = status;
+            }
+            if (((PINF & (1<<afbouw_knop)) == 0) || (Bluetooth_Getal == 4))//Afbouwen
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 9;
+                State_verander = status;
+            }
+            if (((PINF & (1<<neerzetten_knop)) == 0) || (Bluetooth_Getal == 5))//Neerzetten
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 6;
+                State_verander = status;
+            }
+            if ((((PINF & (1<<in_uit_klappen_knop)) == 0) || (Bluetooth_Getal == 8)) && ((PINA & (1<<ingeklapt_switch)) == 0))//Uitklappen maar alleen als die ingeklapt is
             {
                 DEBOUNCE;
                 Bluetooth_Getal = 20;
                 status = 2;
+                State_verander = status;
+            }
+            if ((((PINF & (1<<in_uit_klappen_knop)) == 0) || (Bluetooth_Getal == 8)) && ((PINA & (1<<uitgeklapt_switch)) == 0))//Inklappen maar alleen als die uitgeklapt is
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 12;
+                State_verander = status;
             }
             break;
 
-        case 2: // wacht
+        case 1: //init_state
+            buzzer(0);
+            knipperLichten(0);
+            set_servo_direction(0);    // ingeklapt
+            h_bridge_set_percentage(0);
+            h_bridge_set_percentage2(0);
+
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Opstarten");
+                State_verander = 100;
+            }
+
+            if (((PINF & (1<<in_uit_klappen_knop)) == 0) || (Bluetooth_Getal == 8))//((PINF & (1<<in_uit_klappen)) == 0)
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 2;
+                State_verander = status;
+            }
+            break;
+
+        case 2: // Uitklappen
             h_bridge_set_percentage(80);    // kleine motor
             knipperLichten(1);
             buzzer(1);
 
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Uitklappen");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Uitklappen");
+                State_verander = 100;
+            }
 
             if ((PINA & (1<<uitgeklapt_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 3;
+                State_verander = status;
             }
             break;
 
-        case 3: // stijger_plaatsen
+        case 3: // Uitgeklapt
             h_bridge_set_percentage(0);
             knipperLichten(0);
             buzzer(0);
 
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Plaats steiger");
-
-            if (((PINF & (1<<in_uit_klappen)) == 0) || (Bluetooth_Getal == 8)) //((PINF & (1<<in_uit_klappen)) == 0)        //Weer inklappen
+            if(State_verander == status)
             {
-                DEBOUNCE;
-                status = 12;
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("Plaats steiger");
+                LCD_Naar_Locatie(1, 2);
+                Stuur_LCD_String("of klap weer in");
+                State_verander = 100;
             }
-            if (((PINF & (1<<opbouw_knop)) == 0) || (Bluetooth_Getal == 3))//((PINF & (1<<opbouw_knop)) == 0)       //Opbouwen
+
+            if (((PINF & (1<<in_uit_klappen_knop)) == 0) || (Bluetooth_Getal == 8))        //Weer inklappen
             {
                 DEBOUNCE;
+                Bluetooth_Getal = 20;
+                status = 12;
+                State_verander = status;
+            }
+            if (((PINF & (1<<opbouw_knop)) == 0) || (Bluetooth_Getal == 3))      //Opbouwen
+            {
+                DEBOUNCE;
+                Bluetooth_Getal = 20;
                 status = 4;
+                State_verander = status;
             }
             break;
 
@@ -158,72 +230,104 @@ int main(void)
             buzzer(1); // aan
             knipperLichten(1); // aan
 
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Opbouwen");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Opbouwen");
+                State_verander = 100;
 
-            _delay_ms(500); // om te zorgen dat de servo's uit zijn voor de motor aan gaat
-
-            h_bridge_set_percentage2(80);
+                _delay_ms(500); // om te zorgen dat de servo's uit zijn voor de motor aan gaat
+            }
+            if ((PINA & (1<<home_switch)) != 0)
+            {
+                h_bridge_set_percentage2(-80);
+            }
+            if((PINA & (1<<home_switch)) == 0)
+            {
+                            h_bridge_set_percentage2(80);
+            }
 
             if ((PINA & (1<<top_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 5;
+                State_verander = status;
             }
             break;
 
         case 5: // volgende_stijger
             h_bridge_set_percentage2(0);
             knipperLichten(0); // uit
+            buzzer(0);
 
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 2);
-	    	Stuur_LCD_String("Plaats volgende");
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("steiger");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("Plaats volgende");
+                LCD_Naar_Locatie(4, 2);
+                Stuur_LCD_String("steiger");
+                State_verander = 100;
+            }
 
             if (((PINF & (1<<neerzetten_knop)) == 0) || (Bluetooth_Getal == 5))//((PINF & (1<<neerzetten_knop)) == 0)
             {
                 DEBOUNCE;
+                Bluetooth_Getal = 20;
                 status = 6;
+                State_verander = status;
             }
             break;
 
         case 6: // neerzetten (opbouwen)
             h_bridge_set_percentage2(-50);
-            buzzer(1); // aan
-            knipperLichten(1); // aan
+            buzzer(1);//aan
+            knipperLichten(1);//aan
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Neerzetten");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Neerzetten");
+                State_verander = 100;
+            }
 
             if((PINA & (1<<middel_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 7;
+                State_verander = status;
             }
             break;
 
         case 7: // stijger_opgestapeld
             h_bridge_set_percentage2(0);
-            set_servo_direction(0); // ingeklapt
-            knipperLichten(0); // uit
+            set_servo_direction(0);//ingeklapt
+            knipperLichten(0);//uit
+            buzzer(0);//aan
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Plaats volgende");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("Kies doorbouwen");
+                LCD_Naar_Locatie(3, 2);
+                Stuur_LCD_String("of afbouwen");
+                State_verander = 100;
+            }
 
-            if (((PINF & (1<<opbouw_knop)) == 0) || (Bluetooth_Getal == 3))//((PINF & (1<<opbouw_knop)) == 0)       //Opbouwen
+            if (((PINF & (1<<opbouw_knop)) == 0) || (Bluetooth_Getal == 3))     //Opbouwen
             {
                 DEBOUNCE;
                 status = 8;
+                State_verander = status;
             }
-            if (((PINF & (1<<afbouw_knop)) == 0) || (Bluetooth_Getal == 4))//((PINF & (1<<afbouw_knop)) == 0)       //Afbouwen
+            if (((PINF & (1<<afbouw_knop)) == 0) || (Bluetooth_Getal == 4))       //Afbouwen
             {
                 DEBOUNCE;
                 status = 9;
+                State_verander = status;
             }
             break;
 
@@ -232,15 +336,19 @@ int main(void)
             knipperLichten(1); // aan
             buzzer(1); // aan
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-
-	    	Stuur_LCD_String("Door bouwen");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(2, 1);
+                Stuur_LCD_String("Door bouwen");
+                State_verander = 100;
+            }
 
             if ((PINA & (1<<home_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 4;
+                State_verander = status;
             }
             break;
 
@@ -249,30 +357,41 @@ int main(void)
             knipperLichten(1); // aan
             set_servo_direction(1); // uitgeklapt
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Afbouwen");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Afbouwen");
+                State_verander = 100;
 
-            // moet pas na het uitklappen van de servo's
+                _delay_ms(500); // om te zorgen dat de servo's uit zijn voor de motor aan gaat
+            }
+
             h_bridge_set_percentage2(20);
             if ((PINA & (1<<top_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 10; // steiger weghalen
+                State_verander = status;
             }
            break;
 
         case 10: // stijger_weghalen
             knipperLichten(0); // uit
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Steiger weghalen");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(1, 1);
+                Stuur_LCD_String("Steiger weghalen");
+                State_verander = 100;
+            }
 
             if (((PINF & (1<<neerzetten_knop)) == 0) || (Bluetooth_Getal == 5))//((PINF & (1<<neerzetten_knop)) == 0)
             {
                 DEBOUNCE;
                 status = 11;
+                State_verander = status;
             }
             break;
 
@@ -282,14 +401,19 @@ int main(void)
 
             h_bridge_set_percentage2(-20);
 
-            LCD_Scherm_Leeg();
-            LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Neerzetten");
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Neerzetten");
+                State_verander = 100;
+            }
 
             if ((PINA & (1<<home_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 3;
+                State_verander = status;
             }
             break;
         case 12: //inklappen
@@ -297,18 +421,24 @@ int main(void)
             buzzer(1); // aan
 
             set_servo_direction(0); // ingeklapt
-            _delay_ms(500);
+
+            if(State_verander == status)
+            {
+                LCD_Scherm_Leeg();
+                LCD_Naar_Locatie(4, 1);
+                Stuur_LCD_String("Inklappen");
+                State_verander = 100;
+
+                _delay_ms(500); // om te zorgen dat de servo's uit zijn voor de motor aan gaat
+            }
 
             h_bridge_set_percentage(-80);    // keine motor
-
-            LCD_Scherm_Leeg();
-           	LCD_Naar_Locatie(1, 1);
-	    	Stuur_LCD_String("Inklappen");
 
             if ((PINA & (1<<ingeklapt_switch)) == 0)
             {
                 DEBOUNCE;
                 status = 1;
+                State_verander = status;
             }
             break;
 		}
@@ -326,7 +456,6 @@ ISR(TIMER5_COMPA_vect)          // timer 1 interrupt compare voor knipperlichten
 ISR(TIMER5_COMPB_vect)          // timer 1 interrupt compare voor knipperlichten
 {
     PORTD |= (1<<PD0);                              // buzzer aan
-    buzz++;
 }
 
 ISR(TIMER5_OVF_vect)            // timer 1 interrupt overflow voor knipperlichten
